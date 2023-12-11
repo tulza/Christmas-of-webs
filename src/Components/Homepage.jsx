69c57cfc5182de738ddef9c4fe3c820ba8beebf4
Tulza <40633228+tulza@users.noreply.github.com>
2023-12-11 22:40:28 +1300

    windmill

--- src/Components/Homepage.jsx
+++ src/Components/Homepage.jsx
@@ -7,7 +7,20 @@ const Homepage = () => {
     <>
       <div className="flex h-full w-full flex-col">
         <NavDesktop setPage={setPage} />
-
+        <div className="relative grid aspect-square w-20 grid-cols-2">
+          <div className="overflow-hidden">
+            <div className="h-full w-full translate-y-[50%] rounded-full bg-blue-500" />
+          </div>
+          <div className="overflow-hidden">
+            <div className="h-full w-full  translate-x-[-50%] rounded-full bg-yellow-200" />
+          </div>
+          <div className="overflow-hidden">
+            <div className="h-full w-full translate-x-[50%] rounded-full bg-yellow-200" />
+          </div>
+          <div className="overflow-hidden">
+            <div className="h-full w-full translate-y-[-50%] rounded-full bg-blue-500" />
+          </div>
+        </div>
       </div>
     </>
   );
